b9ec81f7b99d226cc0388c2cd254d6fd88b12a5a
Prakash <prakashchandraaec@gamil.com>
2021-09-28 17:48:12 +0530

    prakash first push

---
+++ src/controllers/address.controller.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const Addresses = require("../models/address.model");
+
+//get all the address details
+router.get("", async (req, res) => {
+  let address = await Addresses.find().lean().exec();
+  res.send({ address });
+});
+
+
+// post user address
+router.post("/createAddress", async (req, res) => {
+  let createdAddress = await User.create(req.body);
+  res.send({ createdAddress });
+});
+
+module.exports = router;
