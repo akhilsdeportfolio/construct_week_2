f1819d89f45883358e3611e8388fb29d40ce8139
Prakash <prakashchandraaec@gamil.com>
2021-09-30 17:03:54 +0530

    commit

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const Addresses = require("../models/address.model");
 //get all the address details
 router.get("", async (req, res) => {
-  let address = await Addresses.find().lean().exec(); 
-  res.render('address.view.ejs', {
-    address: address
-  } );
+  let address = await Addresses.find().lean().exec();
+  res.render("address.view.ejs", {
+    address: address,
+  });
 });
 // post user address
 router.post("", async (req, res) => {
@@ -18,16 +18,37 @@ router.post("", async (req, res) => {
 // delete user address
 router.get("/delete/:id", async (req, res) => {
   let deleteAddress = await Addresses.findByIdAndDelete(req.params.id);
-  res.send({ deleteAddress });
+  res.redirect("/address");
 });
 
-//update address 
+//update a particular address
 router.patch("/updateAddress/:id", async (req, res) => {
-  let updateAddress = await Addresses.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
+  let updateAddress = await Addresses.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
 
   res.send({ updateAddress });
 });
+
+// get a particular user address
+router.get("/singleAddress/:id", async function (req, res) {
+  let singleAddress = await Addresses.find({ user_id: { $eq: req.params.id } })
+    .lean()
+    .exec();
+  res.render("address.view.ejs", {
+    address: singleAddress,
+  });
+
+  //res.send(singleAddress);
+});
+
+// router.get("/singleAddress/:id", async function (req, res) {
+//   let singleAddress = await Addresses.findById(req.params.id).lean().exec();
+//   res.send(singleAddress);
+// });
 
 module.exports = router;
