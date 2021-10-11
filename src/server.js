162ba1ce348ee86edec7793afc006334e302884f
Akhil Kamsala <87219018+akhilsdeportfolio@users.noreply.github.com>
2021-10-11 14:19:14 +0530

    removed firebase config from server js file

--- src/server.js
+++ src/server.js
@@ -27,17 +27,6 @@ const orderNumberController = require("./controllers/orderNumber.controller");
 const orderController = require("./controllers/order.controller");
 
 //this is a firebase config object dont worry about it ;
-const firebaseConfig = {
-  apiKey: "AIzaSyDc34fFMm_hUBM1u0lPpcbQZ3p4t5EWdQE",
-  authDomain: "constructweek2.firebaseapp.com",
-  projectId: "constructweek2",
-  storageBucket: "constructweek2.appspot.com",
-  messagingSenderId: "472880804645",
-  appId: "1:472880804645:web:23f50d61a6497d2fa4a0cc",
-  measurementId: "G-C4Y67ND84R",
-};
-
-const fbApp = firebaseapp.initializeApp(firebaseConfig);
 
 const app = express();
 app.use(express.json()); //for parsing json data
